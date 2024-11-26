1494a3a61cc4b1fe44dcb4761a26464da6527676
Momina Zafar <150460838+notmomi@users.noreply.github.com>
2024-11-26 18:46:12 +0500

    Add files via upload

---
+++ Assignment_01/book.js
@@ -0,0 +1,55 @@
+const Books = [
+    { 
+        id: 10, 
+        title: "The Theory of Everything", 
+        author: "Stephen Hawking", 
+        rating: 4.6, 
+        genre: "Science" 
+    },
+    { 
+        id: 11, 
+        title: "The Hobbit", 
+        author: "J.R.R. Tolkien", 
+        rating: 4.7, 
+        genre: "Fiction" 
+    },
+    { 
+        id: 12, 
+        title: "The Lord of the Rings", 
+        author: "J.R.R. Tolkien", 
+        rating: 4.9, 
+        genre: "Fiction" 
+    },
+    { 
+        id: 13, 
+        title: "Wuthering Heights", 
+        author: "Emily Brontë", 
+        rating: 4.6, 
+        genre: "Romance" 
+    },
+    { 
+        id: 14, 
+        title: "Dune", 
+        author: "Frank Herbert", 
+        rating: 4.8, 
+        genre: "Science" 
+    },
+    { 
+        id: 15, 
+        title: "The Hunger Games", 
+        author: "Suzanne Collins", 
+        rating: 4.5, 
+        genre: "Fiction" 
+    },
+    { 
+        id: 16, 
+        title: "The Notebook", 
+        author: "Nicholas Sparks", 
+        rating: 4.4, 
+        genre: "Romance" 
+    }
+  ];
+  
+  
+  export default Books;
+  
